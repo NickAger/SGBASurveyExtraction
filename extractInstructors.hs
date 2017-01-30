b787dd760cbab77336d29f273e75fad54a8d64c6
Nick Ager <nick.ager@gmail.com>
2017-01-30 10:22:14 +0000

    simplified and works!

--- extractInstructors.hs
+++ extractInstructors.hs
@@ -1,33 +1,27 @@
 #!/usr/bin/env stack
 -- stack runghc --resolver lts-7.15 --install-ghc --package text --package cassava
 
-{-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE DeriveGeneric #-}
 
-import Data.Char
-import qualified Data.Text.IO as T
 import qualified Data.Text as T
-import System.Environment
+import System.Environment as Env
 import Data.Csv
 import qualified Data.Vector as V
 import qualified Data.ByteString.Lazy as BL
 import GHC.Generics
 
-data SurveyFields = SurveyFields { assistantInstructor :: !T.Text , otherInstuctor :: !T.Text , instructorText :: !T.Text , contactDetails :: !T.Text }
+data SurveyFields = SurveyFields { contactDetails :: !T.Text, age :: !T.Text, membershipLength :: !T.Text, assistantInstructor :: !T.Text , otherInstuctor :: !T.Text , instructorText :: !T.Text }
   deriving (Generic, Show)
 
 instance ToNamedRecord SurveyFields
 instance DefaultOrdered SurveyFields
 
 instance FromRecord SurveyFields where
-    parseRecord v = SurveyFields <$> v .! 36 <*> v .! 38 <*> v .! 39 <*> v .! 104
-
-extractFieldsFromLine fields =
-    mconcat [assistantInstructor fields, " ,", otherInstuctor fields, " ,", instructorText fields, " ,", contactDetails fields]
+    parseRecord v = SurveyFields <$> v .! 104 <*> v .! 16 <*> v .! 15 <*> v .! 36 <*> v .! 38 <*> v .! 39
 
 readCSVLines filePath = do
   csvData <- BL.readFile filePath
-  case decode NoHeader csvData  :: Either String (V.Vector SurveyFields) of
+  case decode HasHeader csvData  :: Either String (V.Vector SurveyFields) of
           Left err -> do
             putStrLn err
             return V.empty
@@ -37,14 +31,9 @@ filterBlankSurveyLine :: SurveyFields -> Bool
 filterBlankSurveyLine survey =
   (T.length (assistantInstructor survey) /= 0) || (T.length (otherInstuctor survey) /= 0) || (T.length (instructorText survey) /= 0)
 
-encodeItemsToFile filePath =
-  BL.writeFile filePath . encodeDefaultOrderedByName
-
 main = do
-  [filePath] <- getArgs
-  progName <- getProgName
+  [filePath] <- Env.getArgs
 
   csvLines <- readCSVLines filePath
   let filteredLines = filter filterBlankSurveyLine $ V.toList csvLines
-
-  return (encodeItemsToFile "instructors.csv" filteredLines)
+  BL.putStr $ encodeDefaultOrderedByName filteredLines
